923e3a3bb2409b0902118f5c62eb9f78712c08e6
Hafeez Basha <shaikhafeez500@gmail.com>
2022-12-14 19:02:12 +0530

    Encap

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly rwonly=new RWOnly();
+        //java: name has private access in com.driver.RWOnly
+        rwonly.setName("Hafeez");
+        System.out.println(rwonly.getName());
+    }
   
 }
